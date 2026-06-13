e630588e1380deddf820546fecf60b390ae789e1
ASHUTOSH SINGH <155178478+ashu-94@users.noreply.github.com>
2026-06-13 18:58:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home";
 import Auth from './pages/Auth'
 import { useEffect } from 'react'
 import axios from 'axios'
